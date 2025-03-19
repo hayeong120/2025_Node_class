5b8494d9dacb762680443fdd3ac8f20eaab8f318
hayeong120 <jbg01200249@gmail.com>
2025-03-19 11:39:10 +0900

    라우팅 설정

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-// http 모듈을 불러옵니다.
-const http = require('http');
+// express 모듈을 불러옴
+const express = require('express');
 
-// 서버를 불러옵니다.
-const server = http.createServer((req, res) => {
-    // 응답 헤더 설정
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//Express 애플리케이션을 생성
+const app = express();
 
-    //클라이언트에게 응답 내용 전송
-    res.end('Hello, Node.js!');
+app.use(express.json());
+
+// 라우팅 설정
+app.post('/swag', (req, res) => {
+    // 클라이언트에게 응답내용 전송
+    res.send(req.body);
 });
 
-//서버가 3000번 포트에서 요청을 기다리도록 설정
-server.listen(3000, () => {
+// 서버가 3000번 포트에서 요청을 기다리도록 설정
+app.listen(3000, () => {
     console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
 });
