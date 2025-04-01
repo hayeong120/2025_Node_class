839605dea1729b9b075e29711d4ad803fb466ca9
hayeong120 <jbg01200249@gmail.com>
2025-04-01 10:04:31 +0900

    db 연결 및 dotenv

--- app2.js
+++ app2.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -10,16 +13,31 @@ app.set('views', path.join(__dirname, 'views'));
 
 const travelList = ['뉴욕', '파리', '서울', '도쿄'];
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('MySQL 연결 실패 : ', err);
+        return;
+    }
+    console.log('MySQL에 연결되었습니다.');
+})
+
 app.get('/', (req, res) => {
 
 });
 
 app.get('/travel', (req, res) => {
-    res.render('travel', {travelList});
+    res.render('travel', { travelList });
 });
 
 app.use((req, res) => {
-    
+
 });
 
 app.listen(3000, () => {
